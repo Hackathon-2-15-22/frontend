448356e73ae0f324cdd0210d668c0a99d79c8f1e
Angela Quach <angelayunquach@gmail.com>
2022-02-16 17:16:07 -0500

    set up expense form

---
+++ src/components/AddExpense/AddExpense.js
@@ -0,0 +1,16 @@
+// import React from "react"
+// import ExpenseForm from "./ExpenseForm"
+
+// const AddExpense = () => {
+
+//   return (
+//     <>
+//     <div className="add-expense">
+//       <h1>Expense</h1>
+//       <ExpenseForm />
+//     </div>
+//     </>
+//   )
+// }
+
+// export default AddExpense
