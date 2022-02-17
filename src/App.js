da615e8efb2178ea1e7bfb6a5739677f142997cc
Angela Quach <angelayunquach@gmail.com>
2022-02-17 14:27:31 -0500

    fix errors

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Home from './pages/Home/Home';
 import Onboarding from './pages/Onboarding/Onboarding';
+import Expenses from './components/Home/Expense';
 
 function App() {
 
