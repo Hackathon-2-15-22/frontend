9751a508951c6e6e41b90a546e8c3d1f88085d2a
Angela Quach <angelayunquach@gmail.com>
2022-02-16 20:07:34 -0500

    working add expense

--- src/components/AddExpense/AddExpense.js
+++ src/components/AddExpense/AddExpense.js
@@ -1,16 +1,38 @@
-// import React from "react"
-// import ExpenseForm from "./ExpenseForm"
+import React, { useState } from "react"
+import { createExpense } from "../../utility/expeneService"
+import { useNavigate } from "react-router-dom"
+import ExpenseForm from "./ExpenseForm"
 
-// const AddExpense = () => {
+const AddExpense = () => {
+  const navigate = useNavigate()
+  const [expense, setExpense] = useState('')
 
-//   return (
-//     <>
-//     <div className="add-expense">
-//       <h1>Expense</h1>
-//       <ExpenseForm />
-//     </div>
-//     </>
-//   )
-// }
+  const formData = {
+    expense: expense,
+  }
 
-// export default AddExpense
+  const handleAddExpense = async (e) => {
+    e.preventDefault()
+    try {
+      const newExpense = await createExpense(formData)
+      navigate('/posts')
+    } catch (error) {
+      throw error
+    }
+  }
+
+  return (
+    <>
+    <div className="add-expense">
+      <h1>Expense</h1>
+      <ExpenseForm 
+        expense={expense}
+        setExpense={setExpense}
+        handleAddExpense={handleAddExpense}
+      />
+    </div>
+    </>
+  )
+}
+
+export default AddExpense
