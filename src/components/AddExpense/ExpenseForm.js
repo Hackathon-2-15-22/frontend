a13e4b4d0ae074e59a84cb7fa5d738a68b519fd5
Angela Quach <angelayunquach@gmail.com>
2022-02-16 20:19:28 -0500

    fix add expense format

--- src/components/AddExpense/ExpenseForm.js
+++ src/components/AddExpense/ExpenseForm.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 const ExpenseForm = (props) => {
-  const [expenseList, setExpenseList] = useState([{ category: "", amount: "" }]);
+  const [expenseList, setExpenseList] = useState([
+    { category: "", amount: "" },
+  ]);
 
   const handleExpenseChange = (e, index) => {
     const { name, value } = e.target;
@@ -12,19 +14,17 @@ const ExpenseForm = (props) => {
 
   const handleExpenseRemove = (index) => {
     const list = [...expenseList];
-    console.log("before remove", list)
+    console.log("before remove", list);
     list.splice(index, 1);
-    setExpenseList(list)
-    console.log("after remove", list)
+    setExpenseList(list);
+    console.log("after remove", list);
   };
 
   const handleExpenseAdd = () => {
     setExpenseList([...expenseList, { category: "", amount: "" }]);
   };
 
-  useEffect(() => { 
-
-  }, [expenseList])
+  useEffect(() => {}, [expenseList]);
 
   return (
     <>
@@ -33,7 +33,13 @@ const ExpenseForm = (props) => {
           <label htmlFor="add-expense">Add Expenses</label>
           {expenseList.map((singleExpense, index) => (
             <div key={index} className="expenses">
-                <select name="category" required onChange={(e) => handleExpenseChange(e, index)} value={singleExpense.category}>
+              <div className="category-select">
+                <select
+                  name="category"
+                  required
+                  onChange={(e) => handleExpenseChange(e, index)}
+                  value={singleExpense.category}
+                >
                   <option value="">--Choose a Category--</option>
                   <option value="Housing">Housing</option>
                   <option value="Transportation">Transportation</option>
@@ -62,18 +68,7 @@ const ExpenseForm = (props) => {
                   <option value="Emergency Fund">Emergency Fund</option>
                   <option value="Large Purchases">Large Purchases</option>
                   <option value="Goals">Goals</option>
-                </select> 
-                <div className="first-division">
-                <input
-                  name="amount"
-                  type="number"
-                  id="amount"
-                  placeholder="$0.00"
-                  value={singleExpense.amount}
-                  onChange={(e) => handleExpenseChange(e, index)}
-                  required
-                />
-                {/* <div className="second-division"> */}
+                </select>
                 {expenseList.length !== 1 && (
                   <button
                     type="button"
@@ -83,9 +78,18 @@ const ExpenseForm = (props) => {
                     <span>-</span>
                   </button>
                 )}
-              {/* </div> */}
               </div>
-              
+              <div className="expense-input">
+                <input
+                  name="amount"
+                  type="number"
+                  id="amount"
+                  placeholder="$0.00"
+                  value={singleExpense.amount}
+                  onChange={(e) => handleExpenseChange(e, index)}
+                  required
+                />
+              </div>
               {expenseList.length - 1 === index && expenseList.length < 15 && (
                 <button
                   type="button"
@@ -94,50 +98,11 @@ const ExpenseForm = (props) => {
                 >
                   <span>Add Expense</span>
                 </button>
-              )} 
+              )}
             </div>
           ))}
         </div>
       </form>
-      {/* <input
-          type="number"
-          required
-          name="expense-amt"
-          autoComplete="off"
-          placeholder="$0.00"
-          value={props.question}
-          onChange={(e) => props.setQuestion(e.target.value)}
-        /> */}
-
-{/* <select name="category" required>
-                  <option value="Housing">Housing</option>
-                  <option value="Transportation">Transportation</option>
-                  <option value="Travel">Travel</option>
-                  <option value="Groceries">Groceries</option>
-                  <option value="Dining Out">Dining Out</option>
-                  <option value="Utilities">Utilities</option>
-                  <option value="Cell Phone">Cell Phone</option>
-                  <option value="Pet Food and Care">Pet Food and Care</option>
-                  <option value="Pet Insurance">Pet Insurance</option>
-                  <option value="Clothing and Personal Upkeep">
-                    Clothing and Personal Upkeep
-                  </option>
-                  <option value="Health Insurance">Health Insurance</option>
-                  <option value="Memberships and Subscriptions">
-                    Memberships and Subscriptions
-                  </option>
-                  <option value="Life Insurance">Life Insurance</option>
-                  <option value="Homeowners Insurance">
-                    Homeowners Insurance
-                  </option>
-                  <option value="Entertainment">Entertainment</option>
-                  <option value="Student Loans">Student Loans</option>
-                  <option value="Credit Card Debt">Credit Card Debt</option>
-                  <option value="Retirement">Retirement</option>
-                  <option value="Emergency Fund">Emergency Fund</option>
-                  <option value="Large Purchases">Large Purchases</option>
-                  <option value="Goals">Goals</option>
-                </select> */}
     </>
   );
 };
