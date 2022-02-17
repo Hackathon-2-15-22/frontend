7601e342e79d197e0a241ed8e72b17cf3cd6fdaf
Jan Horak <janny_b_goode@hotmail.com>
2022-02-17 16:58:16 -0500

    goals

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,34 +6,68 @@ import Expense from '../../components/Home/Expense';
 
 import { getAllExpenses } from "../../utility/expenseService";
 import { getAllIncomes } from "../../utility/incomeService";
+import { getAllGoals } from "../../utility/goalService";
 import { getUser } from "../../utility/auth";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Home = ({ user }) => {
 
   let moneySpent = 0;
   let wealth = 0;
+  let goalTotal = 0
+  let towardGoal = 0
 
   const [userExpenses, setUserExpenses] = useState([]);
   const [userWealth, setUserWealth] = useState();
   const [userFundsLeft, setUserFundsLeft] = useState();
+  // const [userGoalTotal, setUserGoalTotal] = useState(0);
+  // const [userSavedForGoal, setUserSavedForGoal] = useState(0);
 
   const calculateExpenses = async () => {
     try {
       const expenseData = await getAllExpenses();
       const userId = await getUser();
       expenseData.forEach((exp) => {
-        console.log(exp.amount)
         if (exp.owner === userId.profile) {
           moneySpent += exp.amount;
         }
       });
-      console.log(moneySpent)
       setUserExpenses(moneySpent);
     } catch (error) {
       throw error;
     }
   };
+  
+  // const calculateSavedTowardGoal = async () => {
+  //   try {
+  //     const expenseData = await getAllExpenses();
+  //     const userId = await getUser();
+  //     expenseData.forEach((exp) => {
+  //       if (exp.owner === userId.profile && exp.category === "Goals") {
+  //         towardGoal+=exp.amount;
+  //       }
+  //     });
+  //     setUserSavedForGoal(towardGoal);
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // };
+
+  // const calculateGoalTotal = async () => {
+  //   try {
+  //     const goalData = await getAllGoals();
+  //     const userId = await getUser();
+  //     goalData.forEach((g) => {
+  //       if (g.owner === userId.profile) {
+  //         goalTotal += g.amount
+  //       }
+  //     });
+  //     setUserGoalTotal(goalTotal);
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // };
+
   const calculateIncomes = async () => {
     try {
       const incomeData = await getAllIncomes();
@@ -50,8 +84,12 @@ const Home = ({ user }) => {
     }
   };
 
-  calculateExpenses()
-  calculateIncomes()
+  useEffect(() => {
+    // calculateSavedTowardGoal()
+    calculateIncomes()
+    // calculateGoalTotal()
+    calculateExpenses()
+  },);
 
     const date = new Date();
     const n = date.toDateString();
@@ -60,7 +98,7 @@ const Home = ({ user }) => {
 
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>Dashboard</h1>
       <h1>Today is {t}</h1>
       <Goal />
       <Income userWealth={userWealth} />
