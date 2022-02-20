9e5cab4cdc76123c879f03320cfbfc72f6a34933
Jan Horak <janny_b_goode@hotmail.com>
2022-02-19 20:29:02 -0500

    add hex color value to catagory data

--- src/components/AddExpense/ExpenseList.js
+++ src/components/AddExpense/ExpenseList.js
@@ -1,18 +1,19 @@
-import React from "react"
+import React from "react";
 
 const ExpenseList = ({ expense, handleDeleteExpense }) => {
-
   return (
     <>
-    <div className="expense-list column">
-      <div className="row">
-       <input  TYPE="text" value={expense.category + "   " + expense.amount}></input>
-        <button onClick={() => handleDeleteExpense(expense._id)}
-        >-</button>
+      <div className="expense-list column">
+        <div className="row">
+          <input
+            TYPE="text"
+            value={expense.category + "   " + expense.amount}
+          ></input>
+          <button onClick={() => handleDeleteExpense(expense._id)}>-</button>
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default ExpenseList
+export default ExpenseList;
