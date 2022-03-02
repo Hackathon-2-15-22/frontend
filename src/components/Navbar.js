3961ab30b302551d12fe55fa883821c37faa3273
Anthony Lofredo <anthony.lofredo@gmail.com>
2022-03-02 12:02:15 -0500

    hamburger navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 const Navbar = (props) => {
     const [navbarOpen, setNavbarOpen] = useState(false)
-    let location = useLocation().pathname    
+    let location = useLocation().pathname
+    const navigate = useNavigate();    
 
     const handleToggle = () => {
         setNavbarOpen(!navbarOpen)
+    }
+
+    const hrStyle = {
+        padding: 0,
+        margin: 0
     }
 
     return (
@@ -29,15 +35,47 @@ const Navbar = (props) => {
                 </>
             ) : (
                 <>
-                <div className="hamburger">
-                    <button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button>
+                    <div className="hamburgerIcon" onClick={handleToggle}>
+                        <i class="fa-solid fa-2xl fa-bars"></i>
+                        {/* {navbarOpen ? "Close" : "Open"} */}
+                    </div>
 
-                    {/* <button className='menu' onClick={props.logout}>
-                        Logout
-                    </button> */}
-                </div>
+                    
                 <div className={`menuNav ${navbarOpen ? "showMenu" : ""}`}>
-                    GHHHH
+                    <div className="hamburgerNav">
+                        <Link to="/">
+                            <div className="title">ea<span className='green'>$</span>e</div>
+                        </Link>
+                        <div className="hamburgerIcon" onClick={() => {
+                            handleToggle();
+                            window.scrollTo(0,0)
+                        }}>
+                            <i class="fa-solid fa-2xl fa-x"></i>
+                        </div>
+                    </div>
+                    <div className="navLinks">
+                        <div className="navLink" onClick={() => {
+                            navigate('/adjust/goals')
+                            handleToggle()}}>
+                            Adjust goals
+                        </div>
+                        <hr style={hrStyle}/>
+                        <div className="navLink" onClick={() => {
+                            navigate('/adjust/income')
+                            handleToggle()}}>
+                            Adjust income
+                        </div>
+                        <hr style={hrStyle}/>
+                        <div className="navLink" onClick={() => {
+                            navigate('/adjust/spending')
+                            handleToggle()}}>
+                            Adjust spending
+                        </div>
+                        <hr style={hrStyle}/>
+                    </div>
+                    <button className='menu navLogout' onClick={props.logout}>
+                        Sign Out
+                    </button>
                 </div>
                 </>
             )}
