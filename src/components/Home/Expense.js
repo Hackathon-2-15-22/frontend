e668b7aba7f8542e149a8434f15ecd422a55d67e
Jan Horak <janny_b_goode@hotmail.com>
2022-03-01 23:16:47 -0500

    refactor home and components

--- src/components/Home/Expense.js
+++ src/components/Home/Expense.js
@@ -4,13 +4,18 @@ import { useNavigate } from "react-router-dom";
 const Expense = (props) => {
   const navigate = useNavigate();
 
+  let userFundsLeft = 0;
+  if (props.userWealth > 0 && props.totalMoneySpent > 0) {
+    userFundsLeft = props.userWealth - props.totalMoneySpent;
+  }
+
   return (
     <>
       <div className="expense">
         <h1>Expense</h1>
         <h3>Your Monthly Budget: $ {props.userWealth}</h3>
-        <h3>You Spent: $ {props.userExpenses}</h3>
-        <h3>You Have: $ {props.userFundsLeft}</h3>
+        <h3>You Spent: $ {props.totalMoneySpent}</h3>
+        <h3>You Have: $ {userFundsLeft}</h3>
         <button onClick={() => navigate("/adjust/spending")}>
           Add Spending
         </button>
--- src/components/Home/Goal.js
+++ src/components/Home/Goal.js
@@ -1,52 +1,12 @@
-import React, { useState } from "react";
-
-import { getAllExpenses } from "../../utility/expenseService";
-import { getAllGoals } from "../../utility/goalService";
-import { getUser } from "../../utility/auth";
+import React from "react";
 
 const Goal = (props) => {
-  let goalTotal = 0;
-  let towardGoal = 0;
-  let percentage = 0
+  let percentage = 0;
 
-  const [userGoalTotal, setUserGoalTotal] = useState(0);
-  const [userSavedForGoal, setUserSavedForGoal] = useState(0);
+  if (props.goalSavedTotal > 0 && props.userGoalTotal > 0) {
+    percentage = (props.goalSavedTotal / props.userGoalTotal) * 100;
+  }
 
-  const calculateGoalTotal = async () => {
-    try {
-      const goalData = await getAllGoals();
-      const userId = await getUser();
-      goalData.forEach((g) => {
-        if (g.owner === userId.profile) {
-          goalTotal += g.amount;
-        }
-      });
-      setUserGoalTotal(goalTotal);
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  const calculateSavedTowardGoal = async () => {
-    try {
-      const expenseData = await getAllExpenses();
-      const userId = await getUser();
-      expenseData.forEach((exp) => {
-        if (exp.owner === userId.profile && exp.category === "Goals") {
-          towardGoal += exp.amount;
-        }
-      });
-      setUserSavedForGoal(towardGoal);
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  calculateSavedTowardGoal();
-  calculateGoalTotal();
-if (userSavedForGoal>0 && userGoalTotal>0){
-  percentage = (userSavedForGoal / userGoalTotal) * 100;
- }
   const goalMessage = () => {
     if (percentage <= 25) {
       return "This is a good start!";
@@ -65,7 +25,7 @@ if (userSavedForGoal>0 && userGoalTotal>0){
       <div className="goal">
         <h1>Goal</h1>
         <h2>
-          ${userSavedForGoal} out of ${userGoalTotal}
+          ${props.goalSavedTotal} out of ${props.userGoalTotal}
         </h2>
         <progress max={100} value={percentage} />
         <button>Adjust Goals</button>
